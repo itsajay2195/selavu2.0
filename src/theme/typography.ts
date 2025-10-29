50e5f43d604aacfe68a50a307e6b2d4850cb9997
Ajaykumar Rajasekaran <ajayrne@gmail.com>
2025-10-29 16:34:01 +0530

    installed required libraries, added themes, created respective folders

---
+++ src/theme/typography.ts
@@ -0,0 +1,17 @@
+export const fontSizes = {
+  display: 36,
+  h1: 32,
+  h3: 24,
+  h5: 18,
+  large: 16,
+  base: 14,
+  small: 13,
+  tiny: 12,
+};
+
+export const fontWeights = {
+  regular: '400',
+  medium: '500',
+  semibold: '600',
+  bold: '700',
+};
